f3e404466f5fdb6bb49e31406212b06feef8c45b
Srujan Kommagiri <kommagiri2003@gmail.com>
2025-08-06 11:06:58 +0000

    update images

--- src/data/work-data.ts
+++ src/data/work-data.ts
@@ -4,7 +4,7 @@ export const projects = [
       title: 'Secure Share',
       description: 'A secure platform for sharing files with end-to-end encryption, ensuring privacy and data protection.',
       tags: ['Next.js', 'File Sharing', 'Encryption', 'Security'],
-      imageUrl: '/secure-share.png',
+      imageUrl: '/secureShare.png',
       imageHint: 'secure file sharing',
       link: 'https://secure-share-gradgear.vercel.app/',
     },
@@ -15,13 +15,5 @@ export const projects = [
       imageUrl: '/agriAssist.png',
       imageHint: 'agriculture technology',
       link: 'https://agri-assist-xi.vercel.app/',
-    },
-    {
-      title: 'Project Alpha',
-      description: 'A cutting-edge e-commerce platform built with Next.js and serverless technologies, focusing on performance and user experience.',
-      tags: ['Next.js', 'React', 'Tailwind CSS', 'Stripe'],
-      imageUrl: '/project-alpha.png',
-      imageHint: 'abstract code',
-      link: '#',
     }
   ];
